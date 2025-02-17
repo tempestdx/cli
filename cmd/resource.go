533e5ad662e3df9a91511ec8b8413a98d7b2bd0a
LissaGreense <walissa13@gmail.com>
2025-02-17 17:06:02 +0100

    Review changes

--- cmd/resource.go
+++ cmd/resource.go
@@ -58,19 +58,37 @@ func listResources(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("create client: %w", err)
 	}
 
-	res, err := tempestClient.ResourceCollectionWithResponse(context.TODO(), appapi.ResourceCollectionJSONRequestBody{
-		Next:        "",
-		RequestType: "list_resources",
-	})
-	if err != nil {
-		return fmt.Errorf("list resources: %w", err)
+	var allResources []appapi.Resource
+	var nextToken string
+
+	for {
+		res, err := tempestClient.ResourceCollectionWithResponse(context.TODO(), appapi.ResourceCollectionJSONRequestBody{
+			Next:        nextToken,
+			RequestType: "list_resources",
+		})
+		if err != nil {
+			return fmt.Errorf("list resources: %w", err)
+		}
+
+		if res.JSON200 == nil {
+			if res.JSON400 != nil {
+				return fmt.Errorf("bad request: %s", res.JSON400.Error)
+			}
+			if res.JSON500 != nil {
+				return fmt.Errorf("server error: %s", res.JSON500.Error)
+			}
+			return fmt.Errorf("unexpected response: %s", res.Status())
+		}
+
+		allResources = append(allResources, res.JSON200.Resources...)
+
+		if res.JSON200.Next == "" {
+			break
+		}
+		nextToken = res.JSON200.Next
 	}
 
-	if res.JSON200 == nil {
-		return fmt.Errorf("unexpected response: %s", res.Status())
-	}
-
-	resources := res.JSON200.Resources
+	resources := allResources
 	if headFlag > 0 && headFlag < len(resources) {
 		resources = resources[:headFlag]
 	} else if tailFlag > 0 && tailFlag < len(resources) {
@@ -112,13 +130,9 @@ func listResources(cmd *cobra.Command, args []string) error {
 	}
 	cmd.Print(out)
 
-	totalCount := len(res.JSON200.Resources)
+	totalCount := len(allResources)
 	if headFlag > 0 || tailFlag > 0 {
 		cmd.Printf("Showing %d of %d resources\n", len(resources), totalCount)
-	}
-
-	if res.JSON200.Next != "" {
-		cmd.Println("More resources available. Use pagination to see more.")
 	}
 
 	return nil
@@ -151,56 +165,88 @@ func getResource(cmd *cobra.Command, args []string) error {
 	}
 
 	if res.JSON200 == nil {
+		if res.JSON400 != nil {
+			return fmt.Errorf("bad request: %s", res.JSON400.Error)
+		}
+		if res.JSON404 != nil {
+			return fmt.Errorf("not found: %s", res.JSON404.Error)
+		}
+		if res.JSON500 != nil {
+			return fmt.Errorf("server error: %s", res.JSON500.Error)
+		}
 		return fmt.Errorf("unexpected response: %s", res.Status())
 	}
 
 	resource := res.JSON200
-	name := " "
+	name := "-"
 	if resource.Name != nil {
 		name = *resource.Name
 	}
 
 	cmd.Printf("Name:\t%s\n", name)
 	cmd.Printf("ID:\t%s\n", resource.Id)
+	externalID := "-"
 	if resource.ExternalId != "" {
-		cmd.Printf("External ID:\t%s\n", resource.ExternalId)
+		externalID = resource.ExternalId
 	}
+	cmd.Printf("External ID:\t%s\n", externalID)
+
+	externalURL := "-"
 	if resource.ExternalUrl != nil {
-		cmd.Printf("External URL:\t%s\n", *resource.ExternalUrl)
+		externalURL = *resource.ExternalUrl
 	}
+	cmd.Printf("External URL:\t%s\n", externalURL)
 	cmd.Println()
 
 	cmd.Println("Metadata:")
 	cmd.Printf("  Type:\t%s\n", resource.Type)
+	orgID := "-"
 	if resource.OrganizationId != nil {
-		cmd.Printf("  Organization ID:\t%s\n", *resource.OrganizationId)
+		orgID = *resource.OrganizationId
 	}
+	cmd.Printf("  Organization ID:\t%s\n", orgID)
+
+	createdBy := "-"
 	if resource.CreatedBy != nil {
-		cmd.Printf("  Created By:\t%s\n", *resource.CreatedBy)
+		createdBy = *resource.CreatedBy
 	}
+	cmd.Printf("  Created By:\t%s\n", createdBy)
+
+	createdAt := "-"
 	if resource.CreatedAt != nil {
-		cmd.Printf("  Creation Timestamp:\t%s\n", resource.CreatedAt.Format(time.RFC3339))
+		createdAt = resource.CreatedAt.Format(time.RFC3339)
 	}
+	cmd.Printf("  Creation Timestamp:\t%s\n", createdAt)
+
+	updatedAt := "-"
 	if resource.UpdatedAt != nil {
-		cmd.Printf("  Last Updated:\t%s\n", resource.UpdatedAt.Format(time.RFC3339))
+		updatedAt = resource.UpdatedAt.Format(time.RFC3339)
 	}
+	cmd.Printf("  Last Updated:\t%s\n", updatedAt)
+
+	syncedAt := "-"
 	if resource.SyncedAt != nil {
-		cmd.Printf("  Last Synced:\t%s\n", resource.SyncedAt.Format(time.RFC3339))
+		syncedAt = resource.SyncedAt.Format(time.RFC3339)
 	}
+	cmd.Printf("  Last Synced:\t%s\n", syncedAt)
 	cmd.Println()
 
-	if resource.Properties != nil {
-		cmd.Println("Properties:")
+	cmd.Println("Properties:")
+	if resource.Properties != nil && len(*resource.Properties) > 0 {
 		for key, value := range *resource.Properties {
 			cmd.Printf("  %s:\t%v\n", key, value)
 		}
-		cmd.Println()
+	} else {
+		cmd.Printf("  -\n")
 	}
+	cmd.Println()
 
+	cmd.Println("Status:")
+	status := "-"
 	if resource.Status != nil {
-		cmd.Println("Status:")
-		cmd.Printf("  Status:\t%s\n", *resource.Status)
+		status = *resource.Status
 	}
+	cmd.Printf("  Status:\t%s\n", status)
 
 	return nil
 }
